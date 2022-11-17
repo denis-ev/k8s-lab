250cb9d77c574267e32627451575b5c906b45fd1
Denis Evers <denis-ev@users.noreply.github.com>
2022-11-17 16:34:51 +0800

    chore: cleanup

--- provision/terraform/proxmox/main.tf
+++ provision/terraform/proxmox/main.tf
@@ -32,7 +32,7 @@ provider "proxmox" {
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "k8s_master" {
-  count = 2                           # just want 1 for now, set to 0 and apply to destroy VM
+  count = 3                           # just want 1 for now, set to 0 and apply to destroy VM
   name  = "master-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
@@ -55,7 +55,7 @@ resource "proxmox_vm_qemu" "k8s_master" {
   disk {
     slot = 0
     # set disk size here. leave it small for testing because expanding the disk takes time.
-    size     = "5G"
+    size     = "20G"
     type     = "scsi"
     storage  = "local-btrfs"
     iothread = 1
@@ -106,14 +106,14 @@ resource "proxmox_vm_qemu" "k8s_worker" {
   cores    = 2
   sockets  = 1
   cpu      = "host"
-  memory   = 2048
+  memory   = 3072
   scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
 
   disk {
     slot = 0
     # set disk size here. leave it small for testing because expanding the disk takes time.
-    size     = "5G"
+    size     = "20G"
     type     = "scsi"
     storage  = "local-btrfs"
     iothread = 1
