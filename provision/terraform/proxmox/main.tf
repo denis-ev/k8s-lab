912c00425f6f215d6b5bbbe4f05a27976d216d2a
Denis Evers <denis-ev@users.noreply.github.com>
2022-11-27 20:45:06 +0800

    chore: testing monitoring grafana

--- provision/terraform/proxmox/main.tf
+++ provision/terraform/proxmox/main.tf
@@ -32,7 +32,7 @@ provider "proxmox" {
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "k8s_master" {
-  count = 3                           # just want 1 for now, set to 0 and apply to destroy VM
+  count = 1                           # just want 1 for now, set to 0 and apply to destroy VM
   name  = "master-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
@@ -48,14 +48,14 @@ resource "proxmox_vm_qemu" "k8s_master" {
   cores    = 2
   sockets  = 1
   cpu      = "host"
-  memory   = 2048
+  memory   = 14336
   scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
 
   disk {
     slot = 0
     # set disk size here. leave it small for testing because expanding the disk takes time.
-    size     = "20G"
+    size     = "100G"
     type     = "scsi"
     storage  = "local-btrfs"
     iothread = 1
@@ -90,7 +90,7 @@ resource "proxmox_vm_qemu" "k8s_master" {
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "k8s_worker" {
-  count = 2                           # just want 1 for now, set to 0 and apply to destroy VM
+  count = 0                           # just want 1 for now, set to 0 and apply to destroy VM
   name  = "worker-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
